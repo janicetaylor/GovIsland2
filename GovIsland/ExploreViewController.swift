42852b7ce784c2e13493a3b881abb51ac9c2664d
Janice Taylor <not.janice.taylor@gmail.com>
2016-04-13 11:36:40 -0400

    Cleaning stuff up

--- GovIsland/ExploreViewController.swift
+++ GovIsland/ExploreViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class ExploreViewController : UIViewController {
-
-    override func viewDidLoad() {
+class ExploreViewController : UITableViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        
+        let selectCell: SelectTableViewCell = tableView.dequeueReusableCellWithIdentifier("SelectCell", forIndexPath: indexPath) as! SelectTableViewCell
+        
+        return selectCell
     }
 
 
