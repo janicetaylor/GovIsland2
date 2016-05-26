d2c0efbd151f94d06f26b28236227fb2056f0613
Janice Taylor <not.janice.taylor@gmail.com>
2016-05-26 16:13:21 -0400

    Cleaning up code

--- GovIsland/ExploreDetailTableViewController.swift
+++ GovIsland/ExploreDetailTableViewController.swift
@@ -42,7 +42,7 @@ class ExploreDetailTableViewController: UITableViewController {
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 210.0/255.0, green: 35.0/255.0, blue: 42.0/255.0, alpha: 1.0)
         self.navigationController!.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "HelveticaNeue-Bold", size: 18.0)!, NSForegroundColorAttributeName:UIColor.whiteColor()]
     }
-    
+
     
     func loadArraysFromPlist() {
         if let path = NSBundle.mainBundle().pathForResource("govisland", ofType: "plist") {
@@ -146,7 +146,6 @@ class ExploreDetailTableViewController: UITableViewController {
 //        [cell.imageView sd_setImageWithURL:[NSURL URLWithString:@"http://www.domain.com/path/to/image.jpg"]
 //            placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
         
-        
         return exploreCell
     }
     
@@ -158,51 +157,4 @@ class ExploreDetailTableViewController: UITableViewController {
         detailViewController.locationDetail = locationArray[indexPath.row]
         self.navigationController?.pushViewController(detailViewController, animated: true)
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
